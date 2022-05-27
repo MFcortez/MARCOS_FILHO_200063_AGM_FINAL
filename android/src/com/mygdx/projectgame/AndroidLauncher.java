3767ca0d919cf8b7bb14c1345da6f990788131fa
Aluno <aluno@LI.LOCAL>
2022-05-26 22:11:13 -0300

    Initial commit

---
+++ android/src/com/mygdx/projectgame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.projectgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.projectgame.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
